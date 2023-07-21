6e0769b35d82d69c6c12bdba1c5f2061158d3a85
Naim Miah <mmiah@fordham.edu>
2023-07-21 16:02:11 -0400

    added something

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -4,4 +4,31 @@ class Array
     self.uniq
   end
 
+
+  # find pairs that add up to zero
+  def two_sum
+    result = []
+    self.each_with_index do |el1, idx1|
+      self.each_with_index do |el2, idx2|
+        if idx2 > idx1 && el1 + el2 == 0
+          result << [idx1, idx2]
+        end
+      end
+    end
+    result
+  end
+
+  def my_transpose
+    result = []
+    self.each_with_index do |row, idx1|
+      row.each_with_index do |col, idx2|
+        result << [idx1, idx2]
+      end
+    end
+    result
+  end
+
 end
+
+p [1, 2, 1, 3, 3].my_uniq
+p [-1, 0, 2, -2, 1].two_sum
